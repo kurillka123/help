7207e94db1e2d47fff2fe9fe615a75872a6af9c1
kurillka123 <145466273+kurillka123@users.noreply.github.com>
2023-12-15 17:49:44 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -7,18 +7,11 @@
 #сортируем только однородные (type) списки
 #for - перебирает элементы
 #range - создает арифметическую прогрессию
-'''
-hero = [1, 4, 8, 10, 20]
 
+
+inventory = [2, 1, 3]
 counter = 0
-while counter < len(hero):
-    print(hero[counter])
-    counter += 1
 
-print(hero)
-'''
-from random import randint
-ranom_digit = []
-for i in range(10):
-    ranom_digit.append(randint(0, 9))
-print(ranom_digit)
+while counter < len (inventory):
+    print(number, inventory[counter])
+    counter += 1
